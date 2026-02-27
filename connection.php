66ea9e26c5580a556dda2fd1d9216be9341e349a
wardahnr <wardahpcy@gmail.com>
2026-02-27 14:51:42 +0700

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$dbname = 'praktikum';
+$username = 'wardahnr';
+$password = 'wardah123';
+    try {
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+        die("Koneksi Database Gagal: " . $e->getMessage());
+    }
+?>
